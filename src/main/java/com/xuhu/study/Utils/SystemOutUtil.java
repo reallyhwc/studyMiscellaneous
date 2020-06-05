d4ed478605bd4ca5d401f52e5d158d07264a05bd
xuhu <reallyhwc@foxmail.com>
2020-06-05 10:07:52 +0800

    提交新增输出工具类

---
+++ src/main/java/com/xuhu/study/Utils/SystemOutUtil.java
@@ -0,0 +1,12 @@
+package com.xuhu.study.Utils;
+
+/**
+ * @author xuhu
+ * @date 2020-06-05 10:05
+ */
+public class SystemOutUtil {
+
+    public static void Separate(){
+        System.out.println("\n----------------------\n");
+    }
+}
