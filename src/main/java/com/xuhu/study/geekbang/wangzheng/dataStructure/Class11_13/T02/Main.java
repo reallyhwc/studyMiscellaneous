a8fbe0cd95d0351e40a5ee10ba1ba87abc164854
xuhu <reallyhwc@foxmail.com>
2020-06-09 19:07:48 +0800

    插入排序，依次往 1,2,3容量的数组中塞新的值，直到容量达到N

---
+++ src/main/java/com/xuhu/study/geekbang/wangzheng/dataStructure/Class11_13/T02/Main.java
@@ -0,0 +1,13 @@
+package com.xuhu.study.geekbang.wangzheng.dataStructure.Class11_13.T02;
+
+import com.xuhu.study.Utils.ListUtils;
+
+/**
+ * @author xuhu
+ * @date 2020-06-09 18:54
+ */
+public class Main {
+    public static void main(String[] args) {
+        ListUtils.pringlnArrays(Solution.sort(new int[]{5,2,3,4,2,1,3,4,2}));
+    }
+}
---
+++ src/main/java/com/xuhu/study/geekbang/wangzheng/dataStructure/Class11_13/T02/Solution.java
@@ -0,0 +1,27 @@
+package com.xuhu.study.geekbang.wangzheng.dataStructure.Class11_13.T02;
+
+/**
+ * @author xuhu
+ * @date 2020-06-08 19:52
+ * 插入排序
+ */
+public class Solution {
+
+    public static int[] sort(int[] arrays){
+        if(arrays == null || arrays.length == 0)return arrays;
+        for(int i = 1; i < arrays.length; i++){
+            int value = arrays[i];
+            int j = i-1;
+            for(; j >= 0; j--){
+                if(arrays[j] > value){
+                    arrays[j+1] = arrays[j];
+                }else {
+                    break;
+                }
+            }
+            arrays[j+1] = value;
+        }
+        return arrays;
+
+    }
+}
