a2e7a0914f4f4ec34b5b44c2d34d8eff31cbffee
xuhu <reallyhwc@foxmail.com>
2020-06-09 19:56:04 +0800

    提交工具类-随机创建二维数组

---
+++ src/main/java/com/xuhu/study/Utils/ArraysUtils.java
@@ -0,0 +1,20 @@
+package com.xuhu.study.Utils;
+
+import java.util.Random;
+
+/**
+ * @author xuhu
+ * @date 2020-06-09 19:25
+ */
+public class ArraysUtils {
+
+    public static void randomlyGeneratedIntDoubleArray(int[][] arrays, int randomMaxValue){
+        int x = arrays.length;
+        int y = arrays[0].length;
+        for(int i = 0; i < x; i++){
+            for(int j = 0; j < y; j++){
+                arrays[i][j] = (int) (Math.random()*randomMaxValue);
+            }
+        }
+    }
+}
