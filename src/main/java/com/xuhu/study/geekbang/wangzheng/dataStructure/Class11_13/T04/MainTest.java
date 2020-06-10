abab1dac7f709ed533418922076b1bcffae45f7b
xuhu <reallyhwc@foxmail.com>
2020-06-10 19:54:12 +0800

    归并排序时间测试

--- src/main/java/com/xuhu/study/geekbang/wangzheng/dataStructure/Class11_13/T04/MainTest.java
+++ src/main/java/com/xuhu/study/geekbang/wangzheng/dataStructure/Class11_13/T04/MainTest.java
@@ -4,6 +4,7 @@ import com.xuhu.study.Utils.ArraysUtils;
 import com.xuhu.study.geekbang.wangzheng.dataStructure.Class11_13.T01.SortBubbleSolution;
 import com.xuhu.study.geekbang.wangzheng.dataStructure.Class11_13.T02.SortInsertSolution;
 import com.xuhu.study.geekbang.wangzheng.dataStructure.Class11_13.T03.SortSelectSolution;
+import com.xuhu.study.geekbang.wangzheng.dataStructure.Class11_13.T05.SortMergeSolution;
 
 /**
  * @author xuhu
@@ -12,8 +13,8 @@ import com.xuhu.study.geekbang.wangzheng.dataStructure.Class11_13.T03.SortSelect
  */
 public class MainTest {
 
-    private static int NUM_OF_ARRAY = 10000;
-    private static int LENGTH_OF_ARRAY = 1000;
+    private static int NUM_OF_ARRAY = 1000;
+    private static int LENGTH_OF_ARRAY = 20000;
     private static int RANDOM_VALUE = 100000;
 
     public static void main(String[] args) {
@@ -21,6 +22,7 @@ public class MainTest {
         int[][] arrays1 = ArraysUtils.randomlyGeneratedIntDoubleArray(NUM_OF_ARRAY,LENGTH_OF_ARRAY,RANDOM_VALUE);
         int[][] arrays2 = ArraysUtils.randomlyGeneratedIntDoubleArray(NUM_OF_ARRAY,LENGTH_OF_ARRAY,RANDOM_VALUE);
         int[][] arrays3 = ArraysUtils.randomlyGeneratedIntDoubleArray(NUM_OF_ARRAY,LENGTH_OF_ARRAY,RANDOM_VALUE);
+        int[][] arrays4 = ArraysUtils.randomlyGeneratedIntDoubleArray(NUM_OF_ARRAY,LENGTH_OF_ARRAY,RANDOM_VALUE);
         long time2 = System.currentTimeMillis();
         long differenceTime1 = time2-time1;
         System.out.println("生成二维数组" + NUM_OF_ARRAY + "*" + LENGTH_OF_ARRAY + "耗时：" + differenceTime1 + "毫秒");
@@ -51,5 +53,14 @@ public class MainTest {
         time2 = System.currentTimeMillis();
         differenceTime1 = time2-time1;
         System.out.println("选择排序结束！总耗时：" + differenceTime1 + "毫秒");
+
+        System.out.println("开始启动插入排序！");
+        time1 = System.currentTimeMillis();
+        for(int i = 0; i < NUM_OF_ARRAY; i++){
+            SortMergeSolution.sort(arrays4[i]);
+        }
+        time2 = System.currentTimeMillis();
+        differenceTime1 = time2-time1;
+        System.out.println("插入排序结束！总耗时：" + differenceTime1 + "毫秒");
     }
 }
---
+++ src/main/java/com/xuhu/study/geekbang/wangzheng/dataStructure/Class11_13/T05/TestTime.java
@@ -0,0 +1,32 @@
+package com.xuhu.study.geekbang.wangzheng.dataStructure.Class11_13.T05;
+
+import com.xuhu.study.Utils.ArraysUtils;
+import com.xuhu.study.geekbang.wangzheng.dataStructure.Class11_13.T03.SortSelectSolution;
+
+/**
+ * @author xuhu
+ * @date 2020-06-10 19:44
+ */
+public class TestTime {
+
+    private static int NUM_OF_ARRAY = 10000;
+    private static int LENGTH_OF_ARRAY = 1000;
+    private static int RANDOM_VALUE = 100000;
+
+    public static void main(String[] args) {
+        long time1 = System.currentTimeMillis();
+        int[][] arrays1 = ArraysUtils.randomlyGeneratedIntDoubleArray(NUM_OF_ARRAY,LENGTH_OF_ARRAY,RANDOM_VALUE);
+        long time2 = System.currentTimeMillis();
+        long differenceTime1 = time2-time1;
+        System.out.println("生成二维数组" + NUM_OF_ARRAY + "*" + LENGTH_OF_ARRAY + "耗时：" + differenceTime1 + "毫秒");
+
+        System.out.println("开始启动插入排序！");
+        time1 = System.currentTimeMillis();
+        for(int i = 0; i < NUM_OF_ARRAY; i++){
+            SortMergeSolution.sort(arrays1[i]);
+        }
+        time2 = System.currentTimeMillis();
+        differenceTime1 = time2-time1;
+        System.out.println("插入排序结束！总耗时：" + differenceTime1 + "毫秒");
+    }
+}
