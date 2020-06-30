3f77279ced259699a59f64bd64e4deab9b29452d
xuhu <reallyhwc@foxmail.com>
2020-06-30 19:58:45 +0800

    获取有序数组中第一个等于目标值的下标

---
+++ src/main/java/com/xuhu/study/geekbang/wangzheng/dataStructure/Class16/T01/FindFirstTargetIndex.java
@@ -0,0 +1,58 @@
+package com.xuhu.study.geekbang.wangzheng.dataStructure.Class16.T01;
+
+/**
+ * @author xuhu
+ * @date 2020-06-30 19:39
+ * 获取有序数组中第一个等于目标值的下标
+ */
+public class FindFirstTargetIndex {
+
+    public int findIndexEasy(int[] nums, int target){
+        int length = nums.length;
+        int begin = 0;
+        int end = length-1;
+        while (begin <= end){
+            int mid = begin + ((end-begin)>>1);
+            if(nums[mid] > target){
+                end = mid-1;
+            }else if (nums[mid] < target){
+                begin = mid+1;
+            }else {
+                if(begin == 0 || nums[begin-1] != target){
+                    return begin;
+                }
+                end = mid-1;
+            }
+        }
+        return -1;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public int findIndex(int[] nums, int target){
+        int begin = 0;
+        int end = nums.length-1;
+        while (begin <= end){
+            int mid = begin + ((end-begin)>>1);
+            if(nums[mid] < target){
+                begin = mid+1;
+            }else {
+                end = mid-1;
+            }
+        }
+        if(begin < nums.length && nums[begin] == target){
+            return begin;
+        }
+        return -1;
+    }
+}
---
+++ src/main/java/com/xuhu/study/geekbang/wangzheng/dataStructure/Class16/T01/TestMain.java
@@ -0,0 +1,14 @@
+package com.xuhu.study.geekbang.wangzheng.dataStructure.Class16.T01;
+
+/**
+ * @author xuhu
+ * @date 2020-06-30 19:52
+ */
+public class TestMain {
+
+    public static void main(String[] args) {
+        FindFirstTargetIndex find = new FindFirstTargetIndex();
+        System.out.println(find.findIndex(new int[]{1,3,4,5,6,8,8,8,11,18},8));
+    }
+
+}
