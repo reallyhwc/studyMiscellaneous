fc2df8c0d4ec6734b5bebbde9d03fa8ed210c136
xuhu <reallyhwc@foxmail.com>
2020-08-03 11:12:37 +0800

    获取有序数组中最后一个等于目标值的下标

---
+++ src/main/java/com/xuhu/study/geekbang/wangzheng/dataStructure/Class16/T02/FindLastTargetIndex.java
@@ -0,0 +1,28 @@
+package com.xuhu.study.geekbang.wangzheng.dataStructure.Class16.T02;
+
+/**
+ * @author xuhu
+ * @date 2020-06-30 19:40
+ * 获取有序数组中最后一个等于目标值的下标
+ */
+public class FindLastTargetIndex {
+
+    public int findIndex(int[] nums, int target) {
+        int length = nums.length;
+        int begin = 0;
+        int end = length - 1;
+        while (begin < end) {
+            int mid = begin + ((end - begin) >> 1);
+            if (mid < target){
+                begin = mid + 1;
+            }else if (mid > target){
+                end = mid - 1;
+            }else {
+                if (mid == length - 1 || nums[mid] < nums[mid+1]){
+                    return mid;
+                }
+            }
+        }
+        return -1;
+    }
+}
---
+++ src/main/java/com/xuhu/study/geekbang/wangzheng/dataStructure/Class16/T02/TestMain.java
@@ -0,0 +1,15 @@
+package com.xuhu.study.geekbang.wangzheng.dataStructure.Class16.T02;
+
+
+/**
+ * @author xuhu
+ * @date 2020-06-30 19:52
+ */
+public class TestMain {
+
+    public static void main(String[] args) {
+        FindLastTargetIndex find = new FindLastTargetIndex();
+        System.out.println(find.findIndex(new int[]{1,3,4,5,6,8,8,8,11,18},8));
+    }
+
+}
