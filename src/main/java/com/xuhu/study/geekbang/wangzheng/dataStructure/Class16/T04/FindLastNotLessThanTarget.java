465395d93d83325a6cc6443f469ef05ae81c8bda
xuhu <reallyhwc@foxmail.com>
2020-08-03 21:20:27 +0800

    查找最后一个小于等于给定值的元素

---
+++ src/main/java/com/xuhu/study/geekbang/wangzheng/dataStructure/Class16/T04/FindLastNotLessThanTarget.java
@@ -0,0 +1,28 @@
+package com.xuhu.study.geekbang.wangzheng.dataStructure.Class16.T04;
+
+/**
+ * @author xuhu
+ * @date 2020-06-30 19:47
+ * 查找最后一个小于等于给定值的元素
+ */
+public class FindLastNotLessThanTarget {
+
+    public int findIndex(int[] nums, int target) {
+        int length = nums.length;
+        int begin = 0;
+        int end = length - 1;
+        while (begin <= end) {
+            int mid = begin + ((end - begin) >> 1);
+            if (nums[mid] < target){
+                begin = mid+1;
+            }else {
+                if (mid == length-1 || nums[mid+1] > nums[mid]){
+                    return mid;
+                }else {
+                    end = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
