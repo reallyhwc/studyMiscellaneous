31e477090fa51c4677ff778c420323bf7aaa4286
xuhu <reallyhwc@foxmail.com>
2020-08-31 18:50:27 +0800

    实现一下二叉查找树的 新增  （并非AVL平衡二叉树）

---
+++ src/main/java/com/xuhu/study/geekbang/wangzheng/dataStructure/Class24/T01/BinarySearchTree.java
@@ -0,0 +1,59 @@
+package com.xuhu.study.geekbang.wangzheng.dataStructure.Class24.T01;
+
+import com.xuhu.study.Utils.TreeNode;
+import com.xuhu.study.Utils.TreeOperation;
+
+/**
+ * @author xuhu
+ * @date 2020-08-31 17:39
+ */
+public class BinarySearchTree {
+
+    private TreeNode node;
+
+    public BinarySearchTree() {
+        node = null;
+    }
+
+    /**
+     * 新增结点
+     * @param value 待新增值
+     */
+    public void addTreeNode(int value){
+        if (node == null){
+            // 如果二叉搜索树为空 则作为根节点
+            node = new TreeNode(value);
+        }else {
+            TreeNode temp = node;
+            while (temp != null){
+                // 循环的往下去匹配，思路不难，看代码理解吧
+                if (temp.val > value){
+                    if (temp.left != null){
+                        temp = temp.left;
+                    }else {
+                        temp.left = new TreeNode(value);
+                        return;
+                    }
+                }else if (temp.val < value) {
+                    if (temp.right != null) {
+                        temp = temp.right;
+                    }else {
+                        temp.right = new TreeNode(value);
+                        return;
+                    }
+                }else {
+                    if (temp.right == null){
+                        temp.right = new TreeNode(value);
+                        return;
+                    }else {
+                        temp = temp.right;
+                    }
+                }
+            }
+        }
+    }
+
+    public void showTree(){
+        TreeOperation.show(this.node);
+    }
+}
---
+++ src/main/java/com/xuhu/study/geekbang/wangzheng/dataStructure/Class24/T01/MainTest.java
@@ -0,0 +1,30 @@
+package com.xuhu.study.geekbang.wangzheng.dataStructure.Class24.T01;
+
+/**
+ * @author xuhu
+ * @date 2020-08-31 17:55
+ */
+public class MainTest {
+
+    public static void main(String[] args) {
+        BinarySearchTree tree = new BinarySearchTree();
+        addTest(tree);
+    }
+
+    public static void addTest(BinarySearchTree tree){
+        tree.addTreeNode(50);
+        tree.addTreeNode(25);
+        tree.addTreeNode(75);
+        tree.addTreeNode(13);
+        tree.addTreeNode(15);
+        tree.addTreeNode(64);
+        tree.addTreeNode(39);
+        tree.addTreeNode(17);
+        tree.addTreeNode(79);
+        tree.addTreeNode(66);
+        tree.addTreeNode(95);
+        tree.addTreeNode(88);
+        tree.addTreeNode(94);
+        tree.showTree();
+    }
+}
