8c1fb94181a53bbb360a176b7a9ea3e7edfc5cb2
xuhu <reallyhwc@foxmail.com>
2020-09-12 16:59:12 +0800

    调整测试用例

--- src/main/java/com/xuhu/study/geekbang/wangzheng/dataStructure/Class28/T01/Main.java
+++ src/main/java/com/xuhu/study/geekbang/wangzheng/dataStructure/Class28/T01/Main.java
@@ -7,22 +7,15 @@ package com.xuhu.study.geekbang.wangzheng.dataStructure.Class28.T01;
 public class Main {
 
     public static void main(String[] args) {
-        BigHeap bigHeap = new BigHeap(10);
-        bigHeap.insert(1);
-        bigHeap.insert(2);
-        bigHeap.insert(3);
-        bigHeap.insert(4);
-        bigHeap.insert(5);
-        bigHeap.insert(6);
-        bigHeap.insert(7);
-        bigHeap.insert(8);
-        bigHeap.insert(9);
-        bigHeap.showHeap();
-        bigHeap.insert(10);
+        int capacity = 1000;
+        BigHeap bigHeap = new BigHeap(capacity);
+        for (int i = 0; i < capacity; i++){
+            bigHeap.insert(i);
+        }
         System.out.println();
         bigHeap.showHeap();
 
-        for (int i = 0; i < 10; i++){
+        for (int i = 0; i < capacity; i++){
             bigHeap.removeTop();
             System.out.println();
             bigHeap.showHeap();
