394d23752efcd28ad4b4b03527bde369ce7e225b
xuhu <reallyhwc@foxmail.com>
2021-02-02 19:44:00 +0800

    排序相关基础类提交

---
+++ src/main/java/com/xuhu/study/geekbang/wangzheng/dataStructure/SpringFestival/D03/Sort.java
@@ -0,0 +1,10 @@
+package com.xuhu.study.geekbang.wangzheng.dataStructure.SpringFestival.D03;
+
+/**
+ * @author xuhu
+ * @date 2021-02-02 19:42
+ */
+public interface Sort {
+
+    int[] sort(int[] array);
+}
